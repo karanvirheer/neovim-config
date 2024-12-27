dd660b415021ece0415574c6cfdbb0535c686d54
karanvirheer <karanvirheer77@gmail.com>
2024-12-26 22:59:30 -0500

    Added cpp DAP support

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,7 +1,20 @@
 local M = {}
 
-M.general = {
+-- DAP Debugging Mapping
+M.dap = {
+  plugin = true,
+  n = {
+    ["<leader>db"] = {
+      "<cmd> DapToggleBreakpoint <CR>",
+      "Add breakpoint at line",
+    },
+    ["<leader>dr"] = {
+      "<cmd> DapContinue <CR>",
+      "Start or continue the debugger",
+    },
+  },
 }
 
-return M
+M.general = {}
 
+return M
